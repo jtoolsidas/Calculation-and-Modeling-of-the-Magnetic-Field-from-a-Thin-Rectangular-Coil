d835d3fc42c32d3c1f42a915caa595c73817b34f
JulianneStratton <72165160+JulianneStratton@users.noreply.github.com>
2024-07-22 16:23:08 -0400

    Update July22ArrayFix.py

--- July22ArrayFix.py
+++ July22ArrayFix.py
@@ -11,3 +11,13 @@ def cot(t):
 
   else:
       return 0
+
+#Other option to work with vector input t
+
+
+def cot(t):
+  if not np.all(np.isclose(np.tan(t), 0.0)):
+    return 1/np.tan(t)
+
+  else:
+      return 0
